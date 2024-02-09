6704ea804e1f01728e1c7d8178fe32250954dad5
Avarfox <Libertacoro@yandex.ru>
2024-02-09 21:23:10 +0300

    prefinal

--- src/s21_matrix_operators.cpp
+++ src/s21_matrix_operators.cpp
@@ -1,79 +1,68 @@
-#include "s21_matrix_oop.hpp"
+/* Copyright [2024] <Casscurs> */
 
-S21Matrix& S21Matrix::operator=(S21Matrix other)
-{
-    swap(other);
-    return *this;
+#include "./s21_matrix_oop.hpp"
+
+S21Matrix& S21Matrix::operator=(S21Matrix other) {
+  swap(other);
+  return *this;
 }
 
-double& S21Matrix::operator()(int i, int j)
-{
-    if (i < 0 || j < 0 || i >= rows_ || j >= cols_)
-        throw std::length_error("incorrect index");
-    return matrix_[i][j];
+double& S21Matrix::operator()(int i, int j) {
+  if (i < 0 || j < 0 || i >= rows_ || j >= cols_)
+    throw std::length_error("incorrect index");
+  return matrix_[i][j];
 }
 
-const double& S21Matrix::operator()(int i, int j) const
-{
-    if (i < 0 || j < 0 || i >= rows_ || j >= cols_)
-        throw std::length_error("incorrect index");
-    return matrix_[i][j];
+const double& S21Matrix::operator()(int i, int j) const {
+  if (i < 0 || j < 0 || i >= rows_ || j >= cols_)
+    throw std::length_error("incorrect index");
+  return matrix_[i][j];
 }
 
-S21Matrix& S21Matrix::operator+=(const S21Matrix& other)
-{
-    SumMatrix(other);
-    return *this;
+S21Matrix& S21Matrix::operator+=(const S21Matrix& other) {
+  SumMatrix(other);
+  return *this;
 }
-S21Matrix& S21Matrix::operator-=(const S21Matrix& other)
-{
-    SubMatrix(other);
-    return *this;
+S21Matrix& S21Matrix::operator-=(const S21Matrix& other) {
+  SubMatrix(other);
+  return *this;
 }
 
-S21Matrix& S21Matrix::operator*=(const S21Matrix& other)
-{
-    MulMatrix(other);
-    return *this;
+S21Matrix& S21Matrix::operator*=(const S21Matrix& other) {
+  MulMatrix(other);
+  return *this;
 }
 
-S21Matrix& S21Matrix::operator*=(const double num)
-{
-    MulNumber(num);
-    return *this;
+S21Matrix& S21Matrix::operator*=(const double num) {
+  MulNumber(num);
+  return *this;
 }
 
-S21Matrix operator+(S21Matrix& matrix1, const S21Matrix& matrix2)
-{
-    matrix1 += matrix2;
-    return matrix1;
+S21Matrix operator+(S21Matrix matrix1, const S21Matrix& matrix2) {
+  matrix1 += matrix2;
+  return matrix1;
 }
 
-S21Matrix operator-(S21Matrix& matrix1, const S21Matrix& matrix2)
-{
-    matrix1 -= matrix2;
-    return matrix1;
+S21Matrix operator-(S21Matrix matrix1, const S21Matrix& matrix2) {
+  matrix1 -= matrix2;
+  return matrix1;
 }
 
-S21Matrix operator*(S21Matrix& matrix1, const S21Matrix& matrix2)
-{
-    matrix1 *= matrix2;
-    return matrix1;
+S21Matrix operator*(S21Matrix matrix1, const S21Matrix& matrix2) {
+  matrix1 *= matrix2;
+  return matrix1;
 }
 
-S21Matrix operator*(S21Matrix& matrix1, const double& num)
-{
-    matrix1 *= num;
-    return matrix1;
+S21Matrix operator*(S21Matrix matrix1, const double& num) {
+  matrix1 *= num;
+  return matrix1;
 }
 
-S21Matrix operator*(const double& num, S21Matrix& matrix1)
-{
-    matrix1 *= num;
-    return matrix1;
+S21Matrix operator*(const double& num, S21Matrix matrix1) {
+  matrix1 *= num;
+  return matrix1;
 }
 
-bool S21Matrix::operator==(const S21Matrix& other) const
-{
-    return EqMatrix(other);
+bool S21Matrix::operator==(const S21Matrix& other) const {
+  return EqMatrix(other);
 }
